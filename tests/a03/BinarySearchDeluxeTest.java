c6df0d480ed76b0eb3a88b0a0454ffeeac311105
Montimur <montimur@gmail.com>
2016-03-07 14:07:22 -0700

    Class exercise

--- tests/a03/BinarySearchDeluxeTest.java
+++ tests/a03/BinarySearchDeluxeTest.java
@@ -7,14 +7,39 @@ import org.junit.Test;
 
 public class BinarySearchDeluxeTest
 {
-
+	String[] strings = {"A", "b", "b", "b", "c", "c", "D", "e"};
 	@Before
 	public void setUp() throws Exception
 	{
+		
 	}
 
 	@Test
-	public void test()
+	public void testFirstIndexOfC()
+	{
+		int index = BinarySearchDeluxe.firstIndexOf(strings, "c", String.CASE_INSENSITIVE_ORDER);
+		
+		assertEquals(4, index);
+	}
+	
+	@Test
+	public void testFirstIndexOfB()
+	{
+		int index = BinarySearchDeluxe.firstIndexOf(strings, "b", String.CASE_INSENSITIVE_ORDER);
+		
+		assertEquals(1, index);
+	}
+	
+	@Test
+	public void testFirstIndexOfA()
+	{
+		int index = BinarySearchDeluxe.firstIndexOf(strings, "A", String.CASE_INSENSITIVE_ORDER);
+		
+		assertEquals(0, index);
+	}
+	
+	@Test
+	public void testLastIndexOf()
 	{
 		fail("Not yet implemented");
 	}
