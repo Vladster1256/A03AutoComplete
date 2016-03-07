d9b6b77c139ee913ed52ea926aac1adb18698a68
Montimur <montimur@gmail.com>
2016-03-07 11:50:00 -0700

    Term Implemented
    I have implemented all the methods for class Term.

--- src/a03/Term.java
+++ src/a03/Term.java
@@ -1,6 +1,9 @@
 package a03;
 
+import java.util.Arrays;
 import java.util.Comparator;
+
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  * 
@@ -8,40 +11,88 @@ import java.util.Comparator;
  *
  */
 
-public class Term implements Comparable<Term> {
+public class Term implements Comparable<Term>
+{
 
-    // Initialize a term with the given query string and weight.
-    public Term(String query, double weight)
-    {
-    	
-    }
+	private final String query;
+	private final double weight;
 
-    // Compare the terms in descending order by weight.
-    public static Comparator<Term> byReverseWeightOrder()
-    {
-		return null;
-    	
-    }
+	// Initialize a term with the given query string and weight.
+	public Term(String query, double weight)
+	{
+		this.query = query;
+		this.weight = weight;
+	}
 
-    // Compare the terms in lexicographic order but using only the first r characters of each query.
-    public static Comparator<Term> byPrefixOrder(int r)
-    {
-		return null;
-    	
-    }
+	// Compare the terms in descending order by weight.
+	public static Comparator<Term> byReverseWeightOrder()
+	{
+		return new ReverseWeightOrder();
 
-    // Compare the terms in lexicographic order by query.
-    public int compareTo(Term that)
-    {
-		return 0;
-    	
-    }
+	}
 
-    // Return a string representation of the term in the following format:
-    // the weight, followed by a tab, followed by the query.
-    public String toString()
-    {
-		return null;
-    	
-    }
+	// Compare the terms in lexicographic order but using only the first r
+	// characters of each query.
+	public static Comparator<Term> byPrefixOrder(int r)
+	{
+		return new PrefixOrder(r);
+
+	}
+
+	// Compare the terms in lexicographic order by query.
+	public int compareTo(Term that)
+	{
+		return query.compareToIgnoreCase(that.query);
+
+	}
+
+	// Return a string representation of the term in the following format:
+	// the weight, followed by a tab, followed by the query.
+	public String toString()
+	{
+		return weight + " " + query;
+	}
+
+	private static class ReverseWeightOrder implements Comparator<Term>
+	{
+		
+		@Override
+		public int compare(Term o1, Term o2)
+		{
+			if (o1.weight > o2.weight)
+				return -1;
+			else if (o1.weight < o2.weight)
+				return 1;
+			else
+				return 0;
+		}
+	}
+	
+	private static class PrefixOrder implements Comparator<Term>
+	{
+		private int n;
+		
+		public PrefixOrder(int r)
+		{
+			n = r;
+		}
+		
+		@Override
+		public int compare(Term o1, Term o2)
+		{
+			return o1.query.substring(0, n+1).compareToIgnoreCase(o2.query.substring(0, n+1));
+		}
+		
+	}
+
+	public static void main(String[] args)
+	{
+		Term[] t = {new Term("China",123.01), new Term("Japan",123.02), new Term("Korea",124.011), new Term("Canada",213.078), new Term("Mexico",210.95)};
+
+		StdOut.println("Default: " + Arrays.toString(t));
+		
+		Arrays.sort(t, byReverseWeightOrder());
+		
+		StdOut.println("Reverse Weight Sort: " + Arrays.toString(t));
+	}
 }
