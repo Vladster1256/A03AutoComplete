5327ad6fe7e431b1bda1cd2e111f074ee993a3f9
Montimur <montimur@gmail.com>
2016-03-08 12:14:20 -0700

    have copied and modified code for lastIndexOf()
    seems to work as well as firstIndexOf().

--- src/a03/BinarySearchDeluxe.java
+++ src/a03/BinarySearchDeluxe.java
@@ -58,13 +58,57 @@ public class BinarySearchDeluxe {
     // Return the index of the last key in a[] that equals the search key, or -1 if no such key.
     public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator)
     {
-		return 0;
-    	
+		int min = 0;
+		int max = a.length - 1;
+		int mid = max / 2;
+		
+		//TODO sort(a);
+
+		if(comparator.compare(key, a[min]) < 0 || comparator.compare(key, a[max]) > 0)
+			return - 1;
+		else if(comparator.compare(key, a[max]) == 0)
+			return max;
+		
+		 do
+		{
+			if(comparator.compare(key, a[mid]) == 0 && comparator.compare(key, a[mid + 1]) < 0)
+			{
+				return mid;
+			}
+			else if(comparator.compare(key, a[mid]) < 0)
+			{
+				max = mid;
+				mid = max / 2;
+				if(min == max)
+				{
+					return - 1;
+				}
+				else
+				{
+					continue;
+				}
+			}
+			else
+			{
+				min = mid;
+				mid += (max - mid) / 2;
+				
+				if(min == max)
+				{
+					return -1;	
+				}else
+				{
+					continue;
+				}
+			}			
+		}while(min != mid);
+		 
+		 return - 1;
     }
     
     public static void main(String[] args)
     {
     	String[] strings = {"a", "a", "b", "c", "d", "d", "e", "e", "e", "f", "h", "i", "k", "k", "l", "m", "o"};
-    	System.out.println(firstIndexOf(strings, "d", String.CASE_INSENSITIVE_ORDER));
+    	System.out.println(lastIndexOf(strings, "j", String.CASE_INSENSITIVE_ORDER));
     }		
 }
